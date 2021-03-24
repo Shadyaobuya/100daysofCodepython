8701ccd82a11af45007878d3873b04e6208de39a
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-03-25 00:39:36 +0300

    simple grading system

---
+++ gradingsystem.py
@@ -0,0 +1,13 @@
+marks=int(input("Enter the average marks of the student  "))
+if marks>=80:
+    print("A")
+elif marks>=70:
+    print("B")
+elif marks>=60:
+    print ("C")
+elif marks>=50:
+    print("D")
+elif marks>=0:
+    print("E")
+else:
+    print("Enter valid marks")
