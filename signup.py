43409288f9700f3e25cb51be0ce9cb65fce02cf9
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-04-06 09:07:13 +0300

    signup

--- signup.py
+++ signup.py
@@ -1,8 +1,8 @@
 def login():
-    UserName=input("Enter Your Username: ")
+    userName=input("Enter Your Username: ")
     password=input("Enter Your password: ")
    
-    if (len(password)<=6)or(password.islower()==True) or (password.isdigit()==True)or (UserName[0:5] == password[0:5]) :
+    if (len(password)<=6)or(password.islower()==True) or (password.isdigit()==True)or (userName[0:5] == password[0:5]) :
         print("Your Password should have At least one Uppercase character,\nA number,\nShouldnt be less than 6 characters\nAnd your Password should not contain your name")  
     else:
         
